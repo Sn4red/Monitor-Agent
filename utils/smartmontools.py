2d633b1aaefea640a1b5e5d5ebe27e7c07a1084c
Sn4red <joaquin.arriaga08@gmail.com>
2025-07-01 22:26:00 -0400

    Implementar logica para Linux en smartmontools.py
    Se agrego la logica para que se pueda ejecutar el comando `smartctl` en
    sistemas Linux. Adicionalmente, se condiciono en requirements.txt para
    que solo se instale el modulo WMI en sistemas Windows, y se corrigio
    un comentario en .gitignore.

--- utils/smartmontools.py
+++ utils/smartmontools.py
@@ -14,15 +14,25 @@ class Smartmontools:
         Ejecuta Smartmontools para obtener las métricas del almacenamiento.
         '''
         try:
-            # * Se obtiene la ruta relativa del ejecutable, y se convierte a
-            # * ruta absoluta para mayor claridad en la depuración.
-            ruta = os.path.abspath(
-                os.path.join(
-                    'externals/smartctl', 'smartctl.exe'
-                )
-            )
+            sistema_operativo = os.name
 
-            comando = [ruta, '-A', almacenamiento, '--device=auto']
+            if sistema_operativo != 'nt':
+                # * Para Windows, se obtiene la ruta relativa del ejecutable, y
+                # * se convierte a ruta absoluta para mayor claridad en la
+                # * depuración.
+                ruta = os.path.abspath(
+                    os.path.join(
+                        'externals/smartctl', 'smartctl.exe'
+                    )
+                )
+
+                comando = [ruta, '-A', almacenamiento, '--device=auto']
+
+            if sistema_operativo == 'nt':
+                # * Para Linux, se asume que smartctl está
+                # * instalado y se ejecuta directamente.
+                comando = ['smartctl', '-A', almacenamiento, '--device=auto']
+
             resultado = subprocess.run(
                 comando, capture_output=True, text=True, check=True
             )
